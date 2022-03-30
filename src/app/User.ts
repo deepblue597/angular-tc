708f21153cd482b09b1374178678231a62fc83b7
jason_k <ikakandris@gmail.com>
2022-03-30 18:49:08 +0300

    first commit

---
+++ src/app/User.ts
@@ -0,0 +1,8 @@
+export interface User {
+    id: Number;
+    name: String;
+    username: String;
+    email: String;
+}
+
+// this is the interface we use for the Users 
---
+++ src/app/components/user-item/user-item.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { User } from '../../User' ; 
+
+@Component({
+  selector: 'app-user-item',
+  templateUrl: './user-item.component.html',
+  styleUrls: ['./user-item.component.css']
+})
+export class UserItemComponent implements OnInit {
+
+  @Input() user: User | undefined ;  // if i understood correctly it is used in order to Input the data 
+
+  
+  constructor() { }
+  
+  
+  ngOnInit(): void {
+  }
+
+}
