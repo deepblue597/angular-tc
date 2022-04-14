ddda14d84c5c2972dea6df65074b4f50c7ff546f
deepblue597 <iasonas2001@hotmail.com>
2022-04-14 11:25:18 +0300

    made dynamically forms by reading the json file

--- src/app/User.ts
+++ src/app/User.ts
@@ -3,6 +3,14 @@ export interface User {
     name: String;
     username: String;
     email: String;
+    address: String[] ; 
+    telephone:Telephone[] ; 
 }
+
+export interface Telephone {
+    cellphone: String ; 
+    stable: String;
+    ide: Number; 
+} 
 
 // this is the interface we use for the Users 
